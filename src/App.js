4e9201288d3396ebd7be4001780150aeb22c5296
Lucy <lucymidgley13@gmail.com>
2020-03-29 11:20:22 -0600

    added storybook, data files and downloaded react table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import "./data/sensors.json"
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+const readings = require("./data/readings.json")
+const sensors = require("./data/sensors.json")
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +20,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        {readings[0].time}
+        {sensors[0].name}
       </header>
     </div>
   );
