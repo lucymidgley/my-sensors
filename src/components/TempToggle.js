46d3ff240672897f2d612f3f1b5c11bc8a0c3d12
Lucy <lucymidgley13@gmail.com>
2020-03-29 20:28:11 -0600

    added stories for temperature toggle

--- src/components/TempToggle.js
+++ src/components/TempToggle.js
@@ -7,13 +7,14 @@ export default function TempToggle(props){
       Temperature: 
   <label className="toggleOption"> {'\u00b0'}C
      <input type="radio"  
-       defaultChecked
+       defaultChecked={props.tempType === "C"}
        value={props.tempType === "C"}
        onChange={()=> props.setTempType("C")} name="tempRadio"/>
    </label>
    <label className="toggleOption"> {'\u00b0'}F
      <input type="radio" 
         value={props.tempType === "F"}
+        defaultChecked={props.tempType === "F"}
        onChange={()=> props.setTempType("F")} name="tempRadio"/>
    </label>
   </div>
---
+++ src/stories/3-TempToggle.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from "@storybook/react";
+import TempToggle from "../components/TempToggle"
+import "../index.scss"
+
+
+storiesOf("TempToggle", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("TempToggle", () => (
+      <TempToggle tempType={"C"} setTempType={action("setTempType")}/>
+    ));
