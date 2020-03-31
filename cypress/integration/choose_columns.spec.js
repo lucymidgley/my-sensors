5477024463b07b7d811879b6619787877dc54997
Lucy <lucymidgley13@gmail.com>
2020-03-30 22:17:12 -0600

    added prettier and formatted cypress tests

--- cypress/integration/choose_columns.spec.js
+++ cypress/integration/choose_columns.spec.js
@@ -1,15 +1,21 @@
 describe("Column choosing", () => {
   beforeEach(() => {
-    cy.visit("/")
-  })
+    cy.visit("/");
+  });
   it("should be able to choose not to render certain columns", () => {
     cy.visit("/");
-    cy.contains("Garage Thermostat").should('exist')
-    cy.contains("All Sensors").should('exist')
-    cy.contains("Name").first().click()
-    cy.contains("Garage Thermostat").should('not.exist')
-    cy.contains("Time").first().click()
-    cy.contains("Value").first().click()
-    cy.contains("All Sensors").should('not.exist')
+    cy.contains("Garage Thermostat").should("exist");
+    cy.contains("All Sensors").should("exist");
+    cy.contains("Name")
+      .first()
+      .click();
+    cy.contains("Garage Thermostat").should("not.exist");
+    cy.contains("Time")
+      .first()
+      .click();
+    cy.contains("Value")
+      .first()
+      .click();
+    cy.contains("All Sensors").should("not.exist");
   });
 });
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,17 +4,23 @@ describe("Navigation", () => {
   });
   it("should visit Temperature sensors", () => {
     cy.visit("/");
-    cy.contains("Temperature Sensors").first().click();
-    cy.contains("Temperature ( \u00b0C )").should('exist')
+    cy.contains("Temperature Sensors")
+      .first()
+      .click();
+    cy.contains("Temperature ( \u00b0C )").should("exist");
   });
   it("should visit Humidity sensors", () => {
     cy.visit("/");
-    cy.contains("Humidity Sensors").first().click();
-    cy.contains("Humidity (%)").should('exist')
+    cy.contains("Humidity Sensors")
+      .first()
+      .click();
+    cy.contains("Humidity (%)").should("exist");
   });
   it("should show information on click", () => {
     cy.visit("/");
-    cy.get("button.Info-button").first().click();
-    cy.contains("Information:").should('exist')
+    cy.get("button.Info-button")
+      .first()
+      .click();
+    cy.contains("Information:").should("exist");
   });
 });
